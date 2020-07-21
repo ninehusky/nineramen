68bc04beeaf2017effdc32932b03c3b6449b3701
Niners <ninerthecreator@gmail.com>
2020-07-20 22:20:22 -0700

    Add error middlewares

--- server/index.js
+++ server/index.js
@@ -4,6 +4,8 @@ const helmet = require('helmet');
 const dotenv = require('dotenv').config();
 const cors = require('cors');
 
+const middlewares = require('./middlewares/middlewares');
+
 const app = express();
 
 app.use(helmet());
@@ -16,6 +18,10 @@ app.use(cors({
 app.get('/', (req, res, next) => {
   res.send("Hello, world!");
 });
+
+app.use(middlewares.notFound);
+app.use(middlewares.errorHandler);
+
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
