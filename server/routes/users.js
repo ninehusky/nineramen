fc63b15133ee9f911a0434e535a9733450d0f001
ninehusky <acheung8@uw.edu>
2020-08-08 23:33:36 -0700

    Implemented promotion/demotion of users

--- server/routes/users.js
+++ server/routes/users.js
@@ -27,4 +27,39 @@ router.get('/:username', passport.authenticate('jwt'), (req, res, next) => {
         });
 });
 
+router.patch('/:username', passport.authenticate('jwt'), async (req, res, next) => {
+    // it might make more sense to include this as an instance method
+    const allowedUpdates = [username, password, ];
+});
+
+router.get('/promote/:username', passport.authenticate('jwt'), async (req, res, next) => {
+    changeUserStatus('admin', req, res, next);
+});
+
+// note that here, admins can demote themselves
+router.get('/demote/:username', passport.authenticate('jwt'), async (req, res, next) => {
+    changeUserStatus('user', req, res, next);
+});
+
+async function changeUserStatus(status, req, res, next) {
+    if (req.user.userType !== 'admin') {
+        return next(new Error('You must be an admin to change the user type of other users!'));
+    }
+    try {
+        const user = await User.findOneAndUpdate({ username: req.params.username }, {
+            userType: status,
+        });
+        if (!user) {
+            return next(new Error('That user does not exist!'));
+        }
+    } catch (error) {
+        return next(error);
+    }
+    res.json({
+        message: `Succesfully changed ${req.params.username}'s status to ${status}.`,
+    });
+}
+
+
+
 module.exports = router;
