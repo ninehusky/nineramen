d7d2f1a98e001effaad850717da5ed67a61872bf
ninehusky <acheung8@uw.edu>
2020-09-01 23:59:22 -0700

    Finished redoing the API wrapper

--- server/users/API.js
+++ server/users/API.js
@@ -3,8 +3,26 @@ const user = require('./user');
 const { User } = user;
 const { userSchema } = user;
 
-console.log(User);
+class UserApiWrapper extends ApiWrapper {
+  async verifyPassword(id, password) {
+    await this.getById(id);
+    return this.model.findById(id).verifyPassword(password);
+  }
+}
 
-const api = new ApiWrapper('User', userSchema, ['password', 'reports']);
+const api = new ApiWrapper('User', userSchema, ['reports']);
 
+async function checkAuthorized(changerId, changeeId) {
+  const changer = await api.getById(changerId);
+  const changee = await api.getById(changeeId);
+  if (changer._id !== changee._id) {
+    if (changee.userType === 'admin' || changer.userType === 'user') {
+      const error = new Error('You are not authorized to make this change.');
+      error.statusCode = 403;
+      throw error;
+    }
+  }
+}
+
+module.exports.checkAuthorized = checkAuthorized;
 module.exports = api;
